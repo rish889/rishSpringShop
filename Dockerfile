3c97b8241a93e14730061f466b9dd2de8efcbc68
Rishabh Garcha <rish889@gmail.com>
2023-02-14 11:41:29 +0530

    pom packaging

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM maven:3.8.6-openjdk-18 as maven_build
+WORKDIR /app
+COPY ./ .
+RUN --mount=type=cache,target=/root/.m2  mvn clean package -f ./pom.xml
+
+FROM openjdk:18
+WORKDIR /app
+COPY --from=maven_build /app/product-service/target/product-service-0.0.1-SNAPSHOT.jar ./target/product-service-0.0.1-SNAPSHOT.jar
+EXPOSE 80
+ENTRYPOINT ["java","-jar","-Dspring.profiles.active=dev","/app/target/product-service-0.0.1-SNAPSHOT.jar"]
--- docker-build.sh
+++ docker-build.sh
@@ -1,10 +1,12 @@
 # sh ./docker-build.sh
-DOCKER_BUILDKIT=1 docker build -f ./DockerfileMain -t 561375666658.dkr.ecr.us-east-1.amazonaws.com/rish-spring-shop:0.0.1-SNAPSHOT .
-docker push 561375666658.dkr.ecr.us-east-1.amazonaws.com/rish-spring-shop:0.0.1-SNAPSHOT
-docker tag 561375666658.dkr.ecr.us-east-1.amazonaws.com/rish-spring-shop:0.0.1-SNAPSHOT 561375666658.dkr.ecr.us-east-1.amazonaws.com/rish-spring-shop:latest
-docker push 561375666658.dkr.ecr.us-east-1.amazonaws.com/rish-spring-shop:latest
+ecr_repo="561375666658.dkr.ecr.us-east-1.amazonaws.com/rish-spring-shop-product-service"
+echo $ecr_repo
+DOCKER_BUILDKIT=1 docker build -f ./Dockerfile -t $ecr_repo:0.0.1-SNAPSHOT .
+#docker push $ecr_repo:0.0.1-SNAPSHOT
+#docker tag $ecr_repo:0.0.1-SNAPSHOT $ecr_repo:latest
+#docker push $ecr_repo:latest
 
 # aws-vault exec rish889IAM --duration=12h
 # $(aws ecr get-login --no-include-email --region us-east-1)
-# docker run -p 8080:80 -t 561375666658.dkr.ecr.us-east-1.amazonaws.com/rish-spring-shop:0.0.1-SNAPSHOT
+# docker run -p 8080:80 -t 561375666658.dkr.ecr.us-east-1.amazonaws.com/rish-spring-shop-product-service:0.0.1-SNAPSHOT
 
