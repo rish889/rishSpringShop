1d33a338e7d93d49f6fd9c4f608e2f2f78ac5ddc
Rishabh Garcha <rish889@gmail.com>
2023-02-14 21:34:25 +0530

    rename

--- cdk/bin/hello-ecs.ts
+++ cdk/bin/hello-ecs.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { HelloEcsStack } from '../lib/hello-ecs-stack';
 
 const app = new cdk.App();
-new HelloEcsStack(app, 'RssEcsStack', {
+new HelloEcsStack(app, 'RishSpringShopStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
