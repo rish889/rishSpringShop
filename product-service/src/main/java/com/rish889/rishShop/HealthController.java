245aa47778af446cda1148857844d22ed865f36e
Rishabh Garcha <rish889@gmail.com>
2023-02-14 10:50:14 +0530

    pom packaging

--- product-service/src/main/java/com/rish889/rishShop/HealthController.java
+++ product-service/src/main/java/com/rish889/rishShop/HealthController.java
@@ -1,5 +1,8 @@
 package com.rish889.rishShop;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +10,14 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/")
+@Slf4j
 public class HealthController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HealthController.class);
 
     @GetMapping
     private Mono<String> healthCheck() {
+        logger.info("Rish Shop Healthy 1");
         return Mono.just("Rish Shop Healthy 1");
     }
 }
