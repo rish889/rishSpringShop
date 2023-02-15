3b48ce92adc5311e95da22d793f46f008cece7f7
Rishabh Garcha <rish889@gmail.com>
2023-02-15 13:29:48 +0530

    cdk rds java

--- cdk/bin/cdk.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from "aws-cdk-lib";
-import { CdkStarterStack } from "../lib/cdk-starter-stack";
-
-const app = new cdk.App();
-// new RishSpringShopStack(app, 'RishSpringShopStack', {});
-
-new CdkStarterStack(app, "cdk-stack", {});
