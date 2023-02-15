6510d21590254e4754832a6efa03e2eb7892ab88
Rishabh Garcha <rish889@gmail.com>
2023-02-15 22:43:09 +0530

    get product by id

--- product-service/src/main/java/com/rish889/rishShop/ProductController.java
+++ product-service/src/main/java/com/rish889/rishShop/ProductController.java
@@ -1,5 +1,8 @@
 package com.rish889.rishShop;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +13,17 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/products")
+@Slf4j
 public class ProductController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HealthController.class);
 
     @Autowired
     private ProductService productService;
 
     @GetMapping("/{productId}")
-    public Mono<ResponseEntity<Product>> getUserById(@PathVariable Long productId) {
+    public Mono<ResponseEntity<Product>> getProductById(@PathVariable Long productId) {
+        logger.info("getProductById : {}", productId);
         Mono<Product> productMono = productService.findById(productId);
         return productMono.map(u -> ResponseEntity.ok(u));
     }
