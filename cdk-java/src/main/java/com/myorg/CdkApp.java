aeeb43f2704e4aa0e90e2c27b3ea5cc92e997261
Rishabh Garcha <rish889@gmail.com>
2023-02-15 10:17:59 +0530

    cdk java

---
+++ cdk-java/src/main/java/com/myorg/CdkApp.java
@@ -0,0 +1,17 @@
+package com.myorg;
+
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.StackProps;
+
+public class CdkApp {
+    public static void main(final String[] args) {
+        App app = new App();
+
+        new EcsStack(app,
+                "RishSpringShopStack",
+                StackProps.builder().build());
+
+        app.synth();
+    }
+}
+
