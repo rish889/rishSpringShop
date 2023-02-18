d2b484cb48c7a491f4878ae2419556c7351bc29c
Rishabh Garcha <rish889@gmail.com>
2023-02-18 19:23:34 +0530

    tests

--- product-service/src/test/java/com/rish889/rishShop/repository/ProductRepositoryTest.java
+++ product-service/src/test/java/com/rish889/rishShop/repository/ProductRepositoryTest.java
@@ -1,41 +1,32 @@
-package com.rish889.rishShop.service;
+package com.rish889.rishShop.repository;
 
 import com.rish889.rishShop.model.Product;
-import com.rish889.rishShop.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
-class ProductServiceTest {
+@DataR2dbcTest
+class ProductRepositoryTest {
 
-    @InjectMocks
-    private ProductService productService;
-
-    @Mock
+    @Autowired
     private ProductRepository productRepository;
 
     @Test
-    public void shouldGetUser() {
-        when(productRepository.findById(1l)).thenReturn(
-                Mono.just(Product.builder()
-                        .productName("iPhone 10")
-                        .build()));
-
-        Mono<Product> actualProductMono = productService.findById(1l);
+    public void whenFindByName_thenReturnEmployee() {
+        Mono<Product> actualProductMono = productRepository.findById(1l);
 
         StepVerifier
                 .create(actualProductMono)
                 .consumeNextWith(actualProduct -> {
                     assertEquals(
-                            Product.builder().productName("iPhone 10").build(),
+                            Product.builder().id(1l).productName("iPhone 10").build(),
                             actualProduct);
                 })
                 .verifyComplete();
