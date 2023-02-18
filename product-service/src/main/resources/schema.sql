1fb58cc08e40eeee4ee5a9381002d5660e668e1f
Rishabh Garcha <rish889@gmail.com>
2023-02-18 16:26:24 +0530

    tests

--- product-service/src/main/resources/schema.sql
+++ product-service/src/main/resources/schema.sql
@@ -1,11 +1,5 @@
 DROP TABLE IF EXISTS products;
 CREATE TABLE IF NOT EXISTS products (
-    id INT AUTO_INCREMENT  PRIMARY KEY,
-    product_name VARCHAR(250) NOT NULL
+    id SERIAL PRIMARY KEY,
+    product_name TEXT NOT NULL
 );
-
---DROP TABLE products;
---CREATE TABLE IF NOT EXISTS products (
---    id SERIAL PRIMARY KEY,
---    product_name TEXT NOT NULL
---);
