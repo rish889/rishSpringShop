806e78019554ab9c5a7c8255c38769fccbd78a8f
Rishabh Garcha <rish889@gmail.com>
2023-02-18 15:25:54 +0530

    tests

--- local-build.sh
+++ local-build.sh
@@ -1,7 +1,6 @@
 JAVA_HOME=/home/rishabh/software/jdk-18.0.2.1_linux-x64_bin/jdk-18.0.2.1
 cd product-service
-mvn clean install -DskipTests
-mvn clean package -DskipTests
+mvn clean package
 java -jar \
 -Dspring.profiles.active=local \
 -Drss.postgres.host=localhost:5432 \
