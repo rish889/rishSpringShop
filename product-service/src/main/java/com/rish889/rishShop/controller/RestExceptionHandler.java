64697c8afad4e8f1961a1e20cecfac82e162daf8
Rishabh Garcha <rish889@gmail.com>
2023-02-19 20:51:54 +0530

    exception handling

--- product-service/src/main/java/com/rish889/rishShop/controller/RestExceptionHandler.java
+++ product-service/src/main/java/com/rish889/rishShop/controller/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.rish889.rishShop.controller;
 
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,13 @@ import reactor.core.publisher.Mono;
 @RestControllerAdvice
 @Slf4j
 class RestExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    Mono<ResponseEntity<ErrorDetails>> constraintViolation(ConstraintViolationException ex) {
+        log.error("ExceptionMessage : {}.", ex.getMessage());
+        return Mono.just(ResponseEntity.badRequest().body(ErrorDetails.builder().message(ex.getMessage()).build()));
+    }
+
     @ExceptionHandler(BadRequestException.class)
     Mono<ResponseEntity<ErrorDetails>> badRequest(BadRequestException ex) {
         log.error("ExceptionMessage : {}.", ex.getMessage());
