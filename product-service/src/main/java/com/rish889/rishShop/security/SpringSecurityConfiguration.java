75053666cbbf71de1cd595f229dcd4775b782ff1
Rishabh Garcha <rish889@gmail.com>
2023-02-20 20:00:21 +0530

    security

--- product-service/src/main/java/com/rish889/rishShop/security/SpringSecurityConfiguration.java
+++ product-service/src/main/java/com/rish889/rishShop/security/SpringSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.rish889.rishShop.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -13,7 +14,7 @@ public class SpringSecurityConfiguration {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf().disable()
-                .httpBasic(httpBasicSpec -> httpBasicSpec.and())
+                .httpBasic(Customizer.withDefaults())
                 .authorizeExchange()
                 .anyExchange().authenticated()
                 .and().formLogin()
