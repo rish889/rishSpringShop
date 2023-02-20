48d3e75f40a5c6ab14e1842422c0e7c5f9c96466
Rishabh Garcha <rish889@gmail.com>
2023-02-20 15:40:28 +0530

    exception handling

--- product-service/src/main/java/com/rish889/rishShop/dto/CreateProductDto.java
+++ product-service/src/main/java/com/rish889/rishShop/dto/CreateProductDto.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +15,6 @@ import lombok.NoArgsConstructor;
 public class CreateProductDto {
     @Schema(example = "iphone4")
     @NotBlank(message = "product_name must not be blank")
-    @Size(min = 1, max = 255, message = "product_name size must be between 1 and 255")
     @JsonProperty("product_name")
     private String productName;
 
