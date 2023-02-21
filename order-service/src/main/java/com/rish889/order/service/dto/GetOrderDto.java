4a905f0ea9c74cd99ed6d0548c9f539f68a1205c
Rishabh Garcha <rish889@gmail.com>
2023-02-21 20:50:13 +0530

    order service

--- order-service/src/main/java/com/rish889/order/service/dto/GetOrderDto.java
+++ order-service/src/main/java/com/rish889/order/service/dto/GetOrderDto.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class GetOrderDto {
 
     @Schema(example = "1")
+    @JsonProperty("order_id")
     private Long id;
 
     @Schema(example = "iphone4")
--- product-service/src/main/java/com/rish889/product/service/dto/GetProductDto.java
+++ product-service/src/main/java/com/rish889/product/service/dto/GetProductDto.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class GetProductDto {
 
     @Schema(example = "1")
+    @JsonProperty("product_id")
     private Long id;
 
     @Schema(example = "iphone4")
