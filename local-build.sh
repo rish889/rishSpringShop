a76acd5eebdfce0c322d1daa4ccbcf955912d222
Rishabh Garcha <rish889@gmail.com>
2023-02-21 18:40:34 +0530

    gatling

--- local-build.sh
+++ local-build.sh
@@ -8,6 +8,8 @@ target/product-service-0.0.1-SNAPSHOT.jar
 
 # mvn -f ./gatling-load-test/pom.xml gatling:test
 
+# DOCKER_BUILDKIT=1 docker build -f ./Dockerfile -t 561375666658.dkr.ecr.us-east-1.amazonaws.com/rish-spring-shop-product-service:0.0.1-SNAPSHOT .
+
 # cd ./cdk-deploy/; cdk synth; cd..
 # cd ./cdk-deploy/; cdk deploy; cd..
 # cd ./cdk-deploy/; cdk destroy; cd..
