b8c4c14e35a0c5d334d81a471b8e1aa379b928c4
Rishabh Garcha <rish889@gmail.com>
2023-02-21 21:18:28 +0530

    order service

--- order-service/src/main/java/com/rish889/order/service/repository/OrderRepository.java
+++ order-service/src/main/java/com/rish889/order/service/repository/OrderRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
 public interface OrderRepository extends ReactiveCrudRepository<Order, Long> {
-    Mono<Order> findById(String id);
+    Mono<Order> findByOrderId(String orderId);
 
 }
--- product-service/src/main/java/com/rish889/product/service/repository/ProductRepository.java
+++ product-service/src/main/java/com/rish889/product/service/repository/ProductRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
 public interface ProductRepository extends ReactiveCrudRepository<Product, Long> {
-    Mono<Product> findById(String id);
+    Mono<Product> findByProductId(String productId);
 
 }
