cd325d9dfed53c86056e38c14da4af9dd71d1f51
Rishabh Garcha <rish889@gmail.com>
2023-02-22 19:07:51 +0530

    product service api

--- order-service/src/main/java/com/rish889/order/service/OrderServiceApplication.java
+++ order-service/src/main/java/com/rish889/order/service/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.rish889.order.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.rish889.adapter", "com.rish889.order"})
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
--- order-service/src/main/java/com/rish889/order/service/service/OrderService.java
+++ order-service/src/main/java/com/rish889/order/service/service/OrderService.java
@@ -1,9 +1,9 @@
 package com.rish889.order.service.service;
 
+import com.rish889.adapter.product.service.client.ProductServiceAdapter;
 import com.rish889.order.service.exception.BadRequestException;
 import com.rish889.order.service.model.Order;
 import com.rish889.order.service.repository.OrderRepository;
-import com.rish889.adapter.product.service.client.ProductServiceAdapter;
 import com.rish889.product.service.dto.GetProductDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,16 +18,16 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
-//    @Autowired
-//    private ProductServiceAdapter productServiceAdapter;
+    @Autowired
+    private ProductServiceAdapter productServiceAdapter;
 
     public Mono<Order> findById(Long productId) {
-        ProductServiceAdapter productServiceAdapter = new ProductServiceAdapter();
         Mono<GetProductDto> getProductDtoMono = productServiceAdapter.getProductByProductId();
-        getProductDtoMono.log();
-        return orderRepository
-                .findById(productId)
-                .switchIfEmpty(Mono.error(new BadRequestException("Product not found. ProductId : " + productId)));
+        return getProductDtoMono
+                .log()
+                .flatMap(getProductDto -> orderRepository
+                        .findById(productId)
+                        .switchIfEmpty(Mono.error(new BadRequestException("Product not found. ProductId : " + productId))));
     }
 
     public Mono<Order> createOrder(Order order) {
